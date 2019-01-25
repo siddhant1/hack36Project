3814d2316c097697233a15344df37d7a7fce4522
shakti97 <shaktisingh230797@gmail.com>
2019-01-26 05:04:35 +0530

    Introduce Routes in app

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,13 +3,20 @@ import Welcome from "../Welcome";
 // import { Select } from "antd";
 import Stream from '../Stream/Stream';
 import Header from "../Header/Header";
+import {Switch,Route} from 'react-router-dom';
+import Content from "../Content/Content";
 
 class Container extends Component {
   render() {
     return (
       <div>
         <Header/>
-        <Stream/>
+          <div className="mainContent container">
+            <Switch >
+                <Route path='/stream' exact component={Stream}/>
+                <Route path='/content' exact component={Content}/>
+            </Switch>
+          </div>
       </div>
     );
   }
