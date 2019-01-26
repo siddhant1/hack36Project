108a3046a7c24d747a197f7af39ad5afd07f1ff1
siddhant <30566406+siddhant1@users.noreply.github.com>
2019-01-26 23:47:49 +0530

    Refactor the App (#11)

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -7,9 +7,7 @@ import { Switch, Route } from "react-router-dom";
 import Content from "../Content/Content";
 import TopicContainer from "../Topic/TopicContainer";
 import SubjectContainer from "../Subject/subjectContainer";
-<<<<<<< HEAD
 import FlashCard from "../FlashCard/FlashCard";
-=======
 import posed, { PoseGroup } from "react-pose";
 
 const RoutesContainer = posed.div({
@@ -17,7 +15,6 @@ const RoutesContainer = posed.div({
   exit: { opacity: 0 }
 });
 
->>>>>>> 304b56b65d140d3a6afe1909c0f833b259f901f2
 class Container extends Component {
   constructor() {
     super();
@@ -25,46 +22,29 @@ class Container extends Component {
     this.msg.volume = 100;
     this.msg.rate = 0.5;
     this.msg.voice = speechSynthesis.getVoices()[0];
+    this.state={
+      isMain : false
+    }
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isMain : true})
+    }, 4500);
   }
   render() {
     return (
       <div>
-<<<<<<< HEAD
-
-        <Header/>
+          {!this.state.isMain ? <Welcome/> : <React.Fragment><Header/>
           <div className="mainContent container">
             <Switch >
-                <Route path='/stream' exact component={Stream}/>
+                <Route path='/' exact component={Stream}/>
                 <Route path='/content' exact component={Content}/>
-                <Route path='/topic' exact component={TopicContainer}/>
-                <Route path='/subject' exact component={SubjectContainer}/>
+                <Route path="/topic" exact render={() => <TopicContainer msg={this.msg} />}/>
+                <Route path="/subject" exact render={() => <SubjectContainer msg={this.msg} />}/>
                 <Route path='/flashy' exact component={FlashCard}/>
             </Switch>
           </div>
-
-=======
-        <Header />
-        <div className="mainContent container">
-          {/* <PoseGroup> */}
-          {/* <RoutesContainer key={location.key}> */}
-          <Switch>
-            <Route path="/stream" exact component={Stream} />
-            <Route path="/content" exact component={Content} />
-            <Route
-              path="/topic"
-              exact
-              render={() => <TopicContainer msg={this.msg} />}
-            />
-            <Route
-              path="/subject"
-              exact
-              render={() => <SubjectContainer msg={this.msg} />}
-            />
-          </Switch>
-          {/* </RoutesContainer> */}
-          {/* </PoseGroup> */}
-        </div>
->>>>>>> 304b56b65d140d3a6afe1909c0f833b259f901f2
+          </React.Fragment>}
       </div>
     );
   }
